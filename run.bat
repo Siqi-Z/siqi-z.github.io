78f76b4913bdfa19d69196e945662c1dcddf1176
Siqi-Z <zsqmastdsml@gmail.com>
2022-08-25 22:39:20 -0400

    JHU

--- run.bat
+++ run.bat
@@ -2,4 +2,5 @@ python jemdoc.py index.jemdoc
 python jemdoc.py bio.jemdoc        
 python jemdoc.py publication.jemdoc
 python jemdoc.py LB.jemdoc
+python jemdoc.py teaching.jemdoc
 python jemdoc.py links.jemdoc
